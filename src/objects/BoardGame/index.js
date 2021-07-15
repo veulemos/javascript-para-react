8385d4fedc6c2866ae7d7792493f3254ec49728e
Veronica Lemos <veulemos@gmail.com>
2021-07-15 18:39:02 -0300

    Add or update style base index

--- src/objects/BoardGame/index.js
+++ src/objects/BoardGame/index.js
@@ -1,10 +1,13 @@
+import "./style.css";
 import CardGame from "../../components/CardGame";
+
 function BoardGame(amountCards){
     const $htmlCardGame = CardGame();
-    const $htmlBoardGame = $htmlCardGame.repeat(amountCards);
-   
-   console.log($htmlBoardGame);
-    return $htmlBoardGame;
+    const $htmlContent = $htmlCardGame.repeat(amountCards);
+    
+    //console.log($htmlContent); mostra as imagens
+    //return $htmlContent; consegue mostrar as imagens
+    return ' <section class="board-game"> ${$htmlContent}</section>';
 }
 
 export default BoardGame;
