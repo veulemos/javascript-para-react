f450be307d6c66bfe4db9b5a974f127d213e9bb5
Veronica Lemos <veulemos@gmail.com>
2021-07-15 19:38:21 -0300

    create PlayerName componet

--- main.js
+++ main.js
@@ -2,8 +2,14 @@ import './src/styles/settings/colors.css';
 import './src/styles/generic/reset.css';
 import './src/styles/elements/base.css';
 import BoardGame from "./src/objects/BoardGame";
+import PlayerName from "./src/components/PlayerName";
 
 const $root = document.querySelector("#root");
-const $htmlBoardGame = BoardGame(6);
-$root.insertAdjacentHTML("beforeend", $htmlBoardGame);
+//const $htmlBoardGame = BoardGame(6);
+//const $htmlPlayerName = PlayerName();
+$root.insertAdjacentHTML(
+    "beforeend", ' ${PlayerName("Player1")} ${PlayerName("Player2")} ${BoardName(6)}'
+    //$htmlPlayerName + $htmlBoardGame
+    
+    );
 
---
+++ src/components/PlayerName/index.js
@@ -0,0 +1,5 @@
+function PlayerName(content){
+    return '<p>${content}</p>';
+}
+
+export default PlayerName;
