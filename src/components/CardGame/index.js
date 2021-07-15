af3a8413d5da1a328d4921f2764df6dfa9f44733
Veronica Lemos <veulemos@gmail.com>
2021-07-15 11:47:36 -0300

    Create matching-game componet

---
+++ src/components/CardGame/index.js
@@ -0,0 +1,8 @@
+import "./style.css";
+
+
+function CardGame() {
+    return '<article class="card-game"><img src= "src/components/images/alura-pixel 1.png" alt="Logo da Alura"></article>';      
+}
+
+export default CardGame;
