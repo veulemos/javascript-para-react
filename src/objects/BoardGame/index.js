751258f93c689d7dfeee2cfde0e8560f0091979d
Veronica Lemos <veulemos@gmail.com>
2021-07-15 14:43:44 -0300

    create BoardGame

---
+++ src/objects/BoardGame/index.js
@@ -0,0 +1,10 @@
+import CardGame from "../../components/CardGame";
+function BoardGame(amountCards){
+    const $htmlCardGame = CardGame();
+    const $htmlBoardGame = $htmlCardGame.repeat(amountCards);
+   
+   console.log($htmlBoardGame);
+    return $htmlBoardGame;
+}
+
+export default BoardGame;
